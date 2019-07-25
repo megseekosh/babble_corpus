da7341f501c3bf784cb4ddf461159e1f0b4dc331
Gladys Baudet <gladys.baudet@gmail.com>
2019-07-25 11:13:40 -0400

    creating part 8 (no annot part 7)

--- utt_and_clip_duration.R
+++ utt_and_clip_duration.R
@@ -3,18 +3,26 @@ library(tidyverse)
 # Opening the linking_file
 babble_linking_file <- read_tsv("/Volumes/pn-opus/Seedlings/ACLEW/aclew_babble_corpus/Linking-file-2-27-19.txt", col_names=F)
 
+
+babble_linking_file %>% 
+  count()
 # Setting the corpus that is currently studied
-corpus <- "Seedlings"
+corpora <- c("Casillas-Yeli", "Tsimane", "Seedlings", "Warlaumont", "Cychosz", "Tseltal")
 
 # Cleaning data and computing durations at the clip level
 babble <- babble_linking_file %>% 
   separate("X3", c("corp","subj","age","begin","end"), sep="_") %>% 
   separate(end, c("end", "wav"), sep=-4) %>% 
   mutate(begin=as.numeric(begin), end=as.numeric(end)) %>% 
-  distinct(corp, subj, begin, end, X4, X5) %>% 
+  distinct(corp, subj, X2, begin, end, X4, X5) %>% 
   mutate(dur_utt = 1000*(end-begin), dur_clip = 1000*(X5-X4)) %>% 
-  filter(corp==corpus)
+  filter(corp %in% corpora)
 
+distinct(babble,X2)
+
+
+babble %>%
+  nrow()
 # Summary of the data at the clip level
 summary(babble)
 sd(babble$dur_clip)
